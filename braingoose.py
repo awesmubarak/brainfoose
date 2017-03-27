1423d21e21c24f1321243f05f448c55949bc7498
Awes Mubarak <awes.mubarak@awesmubarak.com>
2017-03-27 20:50:42 +0100

    Add interactive prompt

--- braingoose.py
+++ braingoose.py
@@ -1,46 +1,53 @@
 #!/usr/bin/env python3
 
-program = input()
-end_index = len(program) - 1
+BOLD = '\033[1m'
+END_BOLD = '\033[0m'
 
-if end_index < 0: end_index = 0
-if end_index >= len(program): end_index = len(program) - 1
-array = [0] * 30000
-pointer = 0
-program_location = 0
+print(BOLD + "\nBraingoose REPL\n" + END_BOLD)
 
-while program_location <= end_index:
-    token = program[program_location]
-    if token == ">":
-        pointer += 1
-    elif token == "<":
-        pointer -= 1
-    elif token == "+":
-        array[pointer] += 1
-    elif token == "-":
-        array[pointer] -= 1
-    elif token == ".":
-        print(chr(array[pointer]), end="")
-    elif token == ",": # wat evn is dis?
-        pass
-    elif token =="[":
-        if array[pointer] == 0:
+while True:
+    program = input("bg >  ")
+    end_index = len(program) - 1
+
+    if end_index < 0: end_index = 0
+    if end_index >= len(program): end_index = len(program) - 1
+    array = [0] * 30000
+    pointer = 0
+    program_location = 0
+
+    while program_location <= end_index:
+        token = program[program_location]
+        if token == ">":
+            pointer += 1
+        elif token == "<":
+            pointer -= 1
+        elif token == "+":
+            array[pointer] += 1
+        elif token == "-":
+            array[pointer] -= 1
+        elif token == ".":
+            print(chr(array[pointer]), end="")
+        elif token == ",": # wat evn is dis?
+            pass
+        elif token =="[":
+            if array[pointer] == 0:
+                loop_level = 1
+                while loop_level > 0:
+                    program_location += 1
+                    character = program[program_location]
+                    if character == "[":
+                        loop_level += 1
+                    elif character == "]":
+                        loop_level -= 1
+        elif token == "]":
             loop_level = 1
             while loop_level > 0:
-                program_location += 1
+                program_location -= 1
                 character = program[program_location]
                 if character == "[":
-                    loop_level += 1
-                elif character == "]":
                     loop_level -= 1
-    elif token == "]":
-        loop_level = 1
-        while loop_level > 0:
+                elif character == "]":
+                    loop_level += 1
             program_location -= 1
-            character = program[program_location]
-            if character == "[":
-                loop_level -= 1
-            elif character == "]":
-                loop_level += 1
-        program_location -= 1
-    program_location += 1
+        program_location += 1
+print("\n")
