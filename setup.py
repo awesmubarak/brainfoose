a065c2a2ba193164e94b62a2f71a10e066ed56cc
Awes Mubarak <awes.mubarak@awesmubarak.com>
2017-11-12 09:37:14 +0000

    Remove changelog
    It added too much confusion.

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-from setuptools import setup
 from codecs import open
 from os import path
+from setuptools import setup
 
 here = path.abspath(path.dirname(__file__))
 
@@ -11,28 +11,28 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as file:
     long_description = file.read()
 
 setup(
-     name='brainfoose',
-     version='2.0.0',
-     description='A brainfuck REPL.',
-     long_description=long_description,
-     url='https://github.com/abactel/brainfoose',
-     author='Awes Mubarak',
-     author_email='abactel@protonmail.com',
-     license='MIT',
-     classifiers=[
-         'Development Status :: 5 - Production/Stable',
-         'Intended Audience :: Developers',
-         'Topic :: Software Development',
-         'Operating System :: OS Independent',
-         'License :: OSI Approved :: MIT License',
-         'Programming Language :: Python'
-     ],
-     keywords='brainfuck esoteric programming language repl',
-     packages=['brainfoose'],
-     entry_points={
+    name='brainfoose',
+    version='2.0.1',
+    description='A brainfuck REPL.',
+    long_description=long_description,
+    url='https://github.com/awesmubarak/brainfoose',
+    author='Awes Mubarak',
+    author_email='awes.mubarak@awesmubarak.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python'
+    ],
+    keywords='brainfuck esoteric programming language repl',
+    packages=['brainfoose'],
+    entry_points={
         "console_scripts": [
             "brainfoose=brainfoose:main"
         ],
-     },
-     install_requires=['docopt', 'termcolor']
+    },
+    install_requires=['docopt', 'termcolor']
 )
